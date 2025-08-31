7d8bd63fd28954ad1b36a3bc88a6985e590f3757
sunwooc55 <sunwooc55@gmail.com>
2025-08-31 15:19:48 +0900

    second commit

--- src/Exercise9_1.java
+++ src/Exercise9_1.java
@@ -5,6 +5,8 @@ class Exercise9_1 {
         System.out.println("c1="+c1);
         System.out.println("c2="+c2);
         System.out.println("c1.equals(c2):"+c1.equals(c2));
+
+        System.out.println("가나다라마바사");
     }
 }
 class SutdaCard {
